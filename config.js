583894aec3eb1dc8617c8b6285ebf1bc05254072
HaoyuanDu <jasonduuuu@hotmail.com>
2021-03-08 21:17:18 -0500

    Ready to Deploy

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  // MONGODB:
+  //   'mongodb+srv://classsed:6lxxxlexaTslHLPe@cluster0-pcsru.mongodb.net/merng?retryWrites=true',
+  MONGODB: 'mongodb+srv://jasonduuuu:a8qSVUoVnOcLWzut@cluster0.smoud.mongodb.net/merng?retryWrites=true&w=majority',
+  SECRET_KEY: 'some very secret key'
+};
